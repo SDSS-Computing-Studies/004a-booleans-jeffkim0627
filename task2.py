ecedc4b77cf156b60f22cc54419fbe490875b675
jeffkim0627 <55156837+jeffkim0627@users.noreply.github.com>
2020-10-01 11:22:24 -0700

    Update task2.py

--- task2.py
+++ task2.py
@@ -14,6 +14,6 @@ number = float(input("Enter the number"))
 if number > 0:
     print("The number is positive")
 elif number == 0:
-    print("The number is 0")
+    print("The number is zero")
 elif number < 0:
     print("The number is negative")
